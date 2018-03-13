84f641a3d8b3d9f13ab5165d1abd7b6ab475dc97
Kaliar <julio.gon.men96@gmail.com>
2018-03-13 10:08:03 -0600

    Diseño de pantalla de creacion de usuario

---
+++ app/src/main/java/mx/itesm/thinkinggreen/CrearUsuario.java
@@ -0,0 +1,13 @@
+package mx.itesm.thinkinggreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrearUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_usuario);
+    }
+}
